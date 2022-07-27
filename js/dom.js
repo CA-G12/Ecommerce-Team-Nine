249ee5ffa6ea505b3c9044c898c82e2fd87bb7b5
fadezak100 <fadyzaqut@gmail.com>
2022-07-27 14:06:47 +0300

    Create a DOM function to render products list

--- js/dom.js
+++ js/dom.js
@@ -1,8 +1,71 @@
-
-
+//Function to create list item
 function creatListElement(item){
+    let products = document.getElementById('products');
+
+
+    let article = document.createElement('article');
+    products.appendChild(article);
+    article.classList.add('item'); 
+    //Image Div
+    let imageDiv = document.createElement('div');
+    article.appendChild(imageDiv);
+    imageDiv.classList.add('img-div');
+    let image = document.createElement('img');
+    imageDiv.appendChild(image);
+    image.setAttribute('src', item.productImg);
+    image.setAttribute('width', "200px" );
+    image.setAttribute('height', "200px");
+
+    let mainSection = document.createElement('section');
+    article.appendChild(mainSection);
+    mainSection.classList.add('products-info');
+
+    let productName = document.createElement('div');
+    mainSection.appendChild(productName);
+    productName.classList.add('products-name');
+    let proudctTitle = document.createElement('p');
+    productName.appendChild(proudctTitle);
+    proudctTitle.textContent = item.productName;
+    productName.classList.add('product-title');
+    let productDescirption = document.createElement('p');
+    productName.appendChild(productDescirption);
+    productDescirption.textContent = item.productDetails;
+
+    let productPrice = document.createElement('div');
+    mainSection.appendChild(productPrice);
+    productPrice.classList.add('product-price');
+
+    let productPriceText = document.createElement('p');
+    productPrice.appendChild(productPriceText);
+    productPriceText.textContent = item.productPrice;
+    productPriceText.classList.add('product-price-title');
+
+    let increaseQuantity = document.createElement('div');
+    productPrice.appendChild(increaseQuantity);
+    increaseQuantity.classList.add('increase-quantity');
+
+    let addButton = document.createElement('button');
+    increaseQuantity.appendChild(addButton);
+    addButton.classList.add('add-to-cart');
+    addButton.textContent = '+';
+    let quantityText = document.createElement('p');
+    increaseQuantity.appendChild(quantityText);
     
+    quantityText.textContent = "1";
+    
+
+    let decressButton = document.createElement('button');
+    increaseQuantity.appendChild(decressButton);
+    decressButton.classList.add('add-to-cart'); 
+    decressButton.textContent = '-';
+    
+
+    let viewMoreButton = document.createElement('button');
+    productPrice.appendChild(viewMoreButton);
+    viewMoreButton.classList.add('view-more-btn');
+    viewMoreButton.setAttribute('src', item.productImg);
 }
+
 
 
 function creatElementDom(item) {
